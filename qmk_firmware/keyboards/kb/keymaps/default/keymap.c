ac040d89df3101fa5b404c10233cf735fcd5f121
jesse <pooljesse@gmail.com>
2017-06-30 23:25:00 -0700

    Relace insert with delete. Remove shift-backspace attempt. Move RGB keys. Move reset to layer 2. Minor corrections.

--- qmk_firmware/keyboards/kb/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/kb/keymaps/default/keymap.c
@@ -11,11 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: Default layer
      *
      * ,-----------------------------------------------------------------------------------.
-     * |Esc^Fn1| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10| F11| F12|PrtS|ScrL| Ins |
+     * |Esc^Fn1| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10| F11| F12|PrtS|ScrL| Del |
      * |-----------------------------------------------------------------------------------|
      * |Esc^~|  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = |   \ |   ` | Home|
      * |-----------------------------------------------------------------------------------|
-     * | Tab  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] | Back^Del | PgUp|
+     * | Tab  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] | Backspace| PgUp|
      * |-----------------------------------------------------------------------------------|
      * |  Ctrl   |  A |  S |  D |  F |  G |  H |  J |  K |  L |  ; |  ' |    Return  | PgDn|
      * |-----------------------------------------------------------------------------------|
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
    KEYMAP(
-      LT(1, KC_ESC), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_INS,
+      LT(1, KC_ESC), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_DEL,
       F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_HOME,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, F(1), KC_PGUP,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,
       KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
       KC_LSFT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END,
       MO(1), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
    KEYMAP(
       KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,
-      KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_MUTE,
+      KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_VOLU,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,
@@ -59,26 +59,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 2: Control layer
      *
      * ,-----------------------------------------------------------------------------------.
-     * |       |    |    |    |    |    |    |    |    |    |    |    |    |    |    | Mode|
+     * |       |    |    |    |    |    |    |    |    |    |    |    |    |    |    |     |
      * |-----------------------------------------------------------------------------------|
-     * |     |    |    |    |    |    |    |    |    |    |    |    |    |     |     | Mode|
+     * |     |    |    |    |    |    |    |    |    |    |    |    |    |     |     |     |
      * |-----------------------------------------------------------------------------------|
-     * |      |    |    |    |    |    |    |    |    |    |    |    |    |          | VA+ |
+     * |      |    |    |    | RST|    |    |    |    |    |    |    |    |          |     |
      * |-----------------------------------------------------------------------------------|
-     * |         |    |    |    |    |    |    |    |    |    |    |    |            | VA- |
+     * |         |    |    |    |    |    |    |    |    |    |    |    |            |     |
      * |-----------------------------------------------------------------------------------|
      * |           |    |    |    |    |    |    |    |    |    |    |          |Sat+|     |
      * |-----------------------------------------------------------------------------------|
-     * |      |  Fn2 |      |          RGB Toggle         |       |       | Hue+|Sat-| Hue-|
+     * |      |  Fn2 | Mode |          RGB Toggle         |  VA-  |  VA+  | Hue-|Sat-| Hue+|
      * `-----------------------------------------------------------------------------------'
      */
    KEYMAP(
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD,
-      KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SAI, KC_TRNS,
-      KC_TRNS, MO(2), KC_TRNS, RGB_TOG, RGB_TOG, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUD, RGB_SAD, RGB_HUI),
+      KC_TRNS, MO(2), RGB_MOD, RGB_TOG, RGB_TOG, RGB_TOG, RGB_VAD, KC_TRNS, RGB_VAI, RGB_HUD, RGB_SAD, RGB_HUI),
 
 };
 
--- qmk_firmware/quantum/version.h
+++ qmk_firmware/quantum/version.h
@@ -1,2 +1,2 @@
-#define QMK_VERSION "a46ee0-dirty"
-#define QMK_BUILDDATE "2017-06-24-21:13:05"
+#define QMK_VERSION "b54e3c-dirty"
+#define QMK_BUILDDATE "2017-06-30-23:15:59"
